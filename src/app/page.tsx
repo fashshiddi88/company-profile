b59a1760a42c1da680e949a8a4888d587fad3224
fashshiddi88 <fajrishidi8@gmail.com>
2025-03-05 16:02:56 +0700

    add background homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,42 @@
 export default function Home() {
   return (
-    <div className="bg-white grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-poppins)]">
-      <main className="flex "></main>
+    <div className="bg-white font-[family-name:var(--font-poppins)]">
+      <main className="relative w-full h-screen">
+        <div
+          className="absolute inset-0 w-full h-full bg-cover bg-center"
+          style={{ backgroundImage: "url('/hero.png')" }}
+        >
+          <div className="absolute inset-0 bg-black/50"></div>
+          <div className="bg-slate-900 opacity-25 p-20 ml-20 mt-100 w-200 h-60 relative text-white z-10"></div>
+          <div className="absolute opacity-50 inset-0 bg-slate-500 ml-40 mt-110 w-160 h-40"></div>
+          <div className="absolute inset-0 ml-41 mt-110">
+            <h1 className="text-9xl font-bold">ArchiHub</h1>
+            <p className="mt-1 text-3xl">Rancang. Bangun. Hidup.</p>
+          </div>
+        </div>
+      </main>
+      <div className="bg-slate-200 h-screen w screen pt-26 px-20 pb-20">
+        <div className="flex gap-x-30">
+          <img src="/design.png" className="w-144 h-144" />
+          <div className="flex flex-col p-5 text-black gap-y-4">
+            <h1 className=" text-6xl">About Us</h1>
+            <p className="text-xl font-[poppins] font-light text-justify">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Perspiciatis fugit quod ex culpa alias incidunt beatae dolor ipsam
+              voluptatum excepturi nisi neque repellat adipisci laborum totam
+              dolorum praesentium dolores fuga nemo sequi, veniam odio tempore!
+              Exercitationem vero, quas laudantium fugit voluptates distinctio
+              minima recusandae delectus laboriosam aliquid quis, atque ab
+              voluptate beatae fugiat quo ea maiores nam tempore numquam sunt
+              nisi eaque. Temporibus laboriosam dicta rem aspernatur veniam
+              incidunt sunt blanditiis id architecto rerum iusto cumque, vero,
+              quia consectetur, excepturi nisi doloribus earum dignissimos sit
+              quaerat illo reprehenderit? Incidunt, dolores voluptatibus. Animi
+              minima quasi tenetur ex labore! Optio, vel odit.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/atomics/navbar.module.tsx
+++ src/components/atomics/navbar.module.tsx
@@ -6,11 +6,12 @@ import { usePathname } from "next/navigation";
 export default function Navbar() {
   const pathname = usePathname();
   return (
-    <div className="w-screen h-20 px-20 bg-white text-black flex justify-between items-center fixed top-0 shadow-md ">
-      <div>
+    <div className="w-screen h-20 px-20 bg-white text-black flex justify-between items-center fixed top-0 shadow-md z-50">
+      <div className="flex gap-x-1 justify-center items center">
+        <img src="/AH.png" className="w-9 h-9" />
         <h1 className="text-3xl text-extrabold font-[playfair]">ArchiHub</h1>
       </div>
-      <div className="flex gap-x-5 font-light">
+      <div className="flex gap-x-8 font-light">
         {[
           { name: "Home", path: "/" },
           { name: "About Us", path: "/about" },
